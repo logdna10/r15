0fac05e18bc4606466fc982daa1228e4e46ad555
logdna69 <72147747+logdna69@users.noreply.github.com>
2023-03-15 20:25:03 +0530

    Add files via upload

---
+++ 2jsvali/js/p3/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const handlers = require("./controller/bookCartHandlers");
+
+const server = express();
+
+server.use(cors());
+server.use(bodyParser());
+
+server.get("/", (req, res, next) => {
+  res.send("hello world");
+});
+
+server.get("/book", handlers.getBooks);
+server.get("/book/:isbn", handlers.getBook);
+server.post("/book", handlers.createBook);
+server.put("/book/:isbn", handlers.updateBook);
+server.delete("/book/:isbn", handlers.deleteBook);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/bookdb")
+  .then((d) => {
+    server.listen(4000, () => {
+      console.log("Server listening at port: 4000");
+    });
+  })
+  .catch((err) => {
+    console.log("ERROR: " + err);
+  });
